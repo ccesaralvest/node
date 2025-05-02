757477190697f839f0884c37b6bf90d7a812008a
Caio Timoteo <ccesaralvest@gmail.com>
2025-05-01 23:07:11 -0300

    Server
    Minimal Node Server, just get a request and see if ther some content, after this, show some text in the end of request

---
+++ ex01/server.js
@@ -0,0 +1,12 @@
+import { createServer } from 'http'
+
+const server = createServer((req, res) => {
+    if(req.url === '/') {
+        res.end('Página Central')
+    }
+    if(req.url === '/about'){
+        res.end("Página Sobre")
+    }
+})
+
+server.listen(3333)
